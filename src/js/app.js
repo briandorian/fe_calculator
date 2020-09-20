a8114744475fe3b3152fc7824e5bea0544e6fd66
Brian Acosta <brian.acosta@xing.com>
2020-09-20 18:42:26 +0200

    Calc functioning more or less fine

--- src/js/app.js
+++ src/js/app.js
@@ -1,72 +1,96 @@
 const buttons = document.querySelectorAll(".calc-button");
 const resultScreen = document.querySelector(".result");
-let buffer = 0;
+let buffer = "0";
+let operator = "";
+let previousOperator= "";
+let total=0;
 
 buttons.forEach(function (number){
     number.addEventListener("click",function(event){ 
         if (isNaN(parseInt(event.target.innerText))){
-            issymbol(event);
+            issymbol(event.target.innerText);
         }else
-            isnumber(event);
+            isnumber(event.target.innerText);
+
+        updateResultScreen();
     });
-    updateResultScreen();
 });
 
 function updateResultScreen(){
     resultScreen.innerText=buffer;
 }
 
-function issymbol(event){
-    switch (event.target.innerText){
+function issymbol(symbol){
+    switch (symbol){
         case "C":
-            clearState(); 
+            clearState();
         break;
         case "<-":
             oneLess();
             break;
         case "X":
-            multiplying(event);
-        break;
-        case "-": 
-        break;
-
+        case "-":
         case "+":
+        case "/":
+            doTheMath(symbol);
         break;
+        case "=":
+            if (previousOperator === null) {
+                // need two numbers to do math
+                return;
+            }
+            flushOperation(parseInt(buffer));
+            previousOperator = null;
+            buffer = +total;
+            total = 0;
+            break;
     }
 }
 
-function isnumber(event){
-    console.log("Entra al isnumber ");
-    if (document.querySelector(".result").innerHTML === "0")
-        resultScreen.innerHTML = event.target.innerText;
-     else 
-        resultScreen.innerHTML += event.target.innerText;
+function isnumber(value){
+    if (buffer === "0")
+        buffer = value;
+    else
+        buffer += value;
 }
 
 function clearState() {
     buffer = 0;
-    document.querySelector(".result").innerHTML = "0";
 }
 
-
 function oneLess() {
-    if (document.querySelector(".result").innerText !== "0"){
-        let length = document.querySelector(".result").innerHTML.length;
-        let newResult = document.querySelector(".result").innerHTML.substring(length-1, 0);
+    if (buffer !== "0"){
+        let length = resultScreen.innerHTML.length;
+        let newResult = resultScreen.innerHTML.substring(length-1, 0);
         if (length <= 1){
-            return document.querySelector(".result").innerHTML = "0";
+            buffer = "0";
         }else
-            return document.querySelector(".result").innerHTML = newResult;
+            buffer = newResult;
     }
 }
 
-function multiplying(event) {
-    if (buffer !== 0){
-        numberInScreen = parseInt(document.querySelector(".result").innerHTML);
-        document.querySelector(".result").innerHTML = (buffer * numberInScreen).toString();
-        buffer = 0;
-    }else{
-        buffer = parseInt(document.querySelector(".result").innerHTML);
-        document.querySelector(".result").innerHTML = "0";
+function doTheMath(value) {
+    if (buffer === "0"){
+        return;
+    }
+    let innerBuffer = parseInt(buffer);
+    if (total === 0)
+        total = innerBuffer;
+    else    
+        flushOperation(innerBuffer);
+
+    previousOperator = value;
+    buffer = "0";
+}
+
+function flushOperation(innerBuffer) {
+    if (previousOperator === "+") {
+        total += innerBuffer;
+    } else if (previousOperator === "-") {
+        total -= innerBuffer;
+    } else if (previousOperator === "X") {
+        total *= innerBuffer;
+    } else {
+        total /= innerBuffer;
     }
 }
