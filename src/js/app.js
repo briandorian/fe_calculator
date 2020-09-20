85dec38061b9e728f52d75e59673a0b1091d9e2a
Brian Acosta <brian.acosta@xing.com>
2020-09-20 17:55:59 +0200

    rethinking of the logic to use less code

--- src/js/app.js
+++ src/js/app.js
@@ -1,74 +1,72 @@
-var buttons = document.querySelectorAll(".calc-button");
-let resultado = document.querySelector(".result").innerHTML;
+const buttons = document.querySelectorAll(".calc-button");
+const resultScreen = document.querySelector(".result");
+let buffer = 0;
 
 buttons.forEach(function (number){
-    number.addEventListener("click",function(){  
-        switch (event.target.innerText) {
-            case "+":
-                addition();
-                break;
-            case "-":
-                substraction();
-                break;
-            case "<-":
-                oneLess();
-                break;
-            case "/":
-                dividing();
-                break;
-            case "X":
-                multiplying();
-                break;
-            case "C":
-                refresh();
-                break;
-            case "=":
-                showResult();
-                break;
-            default:
-                appendNumberToResult(this.innerText);
-        }  
+    number.addEventListener("click",function(event){ 
+        if (isNaN(parseInt(event.target.innerText))){
+            issymbol(event);
+        }else
+            isnumber(event);
     });
+    updateResultScreen();
 });
 
+function updateResultScreen(){
+    resultScreen.innerText=buffer;
+}
+
+function issymbol(event){
+    switch (event.target.innerText){
+        case "C":
+            clearState(); 
+        break;
+        case "<-":
+            oneLess();
+            break;
+        case "X":
+            multiplying(event);
+        break;
+        case "-": 
+        break;
+
+        case "+":
+        break;
+    }
+}
+
+function isnumber(event){
+    console.log("Entra al isnumber ");
+    if (document.querySelector(".result").innerHTML === "0")
+        resultScreen.innerHTML = event.target.innerText;
+     else 
+        resultScreen.innerHTML += event.target.innerText;
+}
+
+function clearState() {
+    buffer = 0;
+    document.querySelector(".result").innerHTML = "0";
+}
+
+
 function oneLess() {
-    if (resultado !== "0"){
+    if (document.querySelector(".result").innerText !== "0"){
         let length = document.querySelector(".result").innerHTML.length;
         let newResult = document.querySelector(".result").innerHTML.substring(length-1, 0);
-        if (length === 1){
+        if (length <= 1){
             return document.querySelector(".result").innerHTML = "0";
         }else
             return document.querySelector(".result").innerHTML = newResult;
     }
 }
-function refresh(){
-    resultado = "0";
-    return document.querySelector(".result").innerHTML = "0";
-}
-function addition() {
-    resultado = event.target.innerText+0 + resultado;
-    document.querySelector(".result").innerHTML = "0";
-    return document.querySelector(".result").innerHTML = resultado.getText;
-}
-function substraction() {
-    return document.querySelector(".result").innerHTML = "0";
-}
-function dividing() {
-    return document.querySelector(".result").innerHTML = "0";
-}
-function multiplying() {
-    return document.querySelector(".result").innerHTML = "0";
-}
-function showResult() {
-    return document.querySelector(".result").innerHTML = "0";
-}
-function appendNumberToResult() {
-    if (resultado === "0")
-    {
-        resultado=event.target.innerText;
-        return document.querySelector(".result").innerHTML = event.target.innerText;
+
+function multiplying(event) {
+    if (buffer !== 0){
+        numberInScreen = parseInt(document.querySelector(".result").innerHTML);
+        document.querySelector(".result").innerHTML = (buffer * numberInScreen).toString();
+        buffer = 0;
+    }else{
+        buffer = parseInt(document.querySelector(".result").innerHTML);
+        document.querySelector(".result").innerHTML = "0";
     }
-    else if (resultado+0 > 0)
-        return resultado= document.querySelector(".result").innerHTML = resultado+event.target.innerText;
-        
 }
